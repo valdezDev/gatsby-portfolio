107ba5d3c2d3108afefdd13f72c179a57eff6f55
Paul Valdez <pvaldez11295@gmail.com>
2019-02-08 14:45:35 -0500

    configured title, cleaned up the gatsby homepage and workspace, added pages

---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const IndexPage = () => (
+  <Layout>
+    <SEO title="Paul Valdez" keywords={[`gatsby`, `application`, `react`]} />
+    <h1>Paul Valdez</h1>
+  </Layout>
+)
+
+export default IndexPage
