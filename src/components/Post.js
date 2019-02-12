9c112689648309d46d50b0558790b149d8c03804
Paul Valdez <pvaldez11295@gmail.com>
2019-02-12 17:12:03 -0500

    added blog page posts, still working on filtering out projects and blog posts

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,45 +1,28 @@
 import React from 'react';
 import { Link } from 'gatsby';
-import { Card, CardTitle, CardText, CardSubtitle, CardBody, Badge } from 'reactstrap';
+import { Card, CardTitle, CardText, CardBody } from 'reactstrap';
 import Img from 'gatsby-image';
-import { slugify } from '../util/utilityFunctions';
+//import { slugify } from '../util/utilityFunctions';
+import '../styles/index.scss';
 
-const Post = ({ title, author, slug, date, body, fluid, tags }) => (
-    <Card>
-      <Link to={slug}>
-        <Img className="card-image-top" fluid={fluid} />
-      </Link>
-      <CardBody>
-        <CardTitle>
-          <Link to={slug}>
-            {title}
-          </Link>
-        </CardTitle>
-        <CardSubtitle>
-          <span className="text-info">{date}</span> by{' '}
-          <span className="text-info">{author}</span>
-        </CardSubtitle>
-      <CardText>{body}</CardText>
-      <ul className="post-tags">
-        {tags.map(tag => (
-          <li key={tag}>
-            <Link to={`/tag/${slugify(tag)}`}>
-              <Badge color="primary" className="text-uppercase">
-                {tag}
-              </Badge>
-            </Link>
-          </li>
-        ))}
-      </ul>
-      
+const Post = ({ title, body, fluid, path }) => (
+  <Card>
 
-        <Link
-          to={slug}
-          className="btn btn-outline-primary float-right">
-          Read More
+      {/*<Img className="card-image-top" fluid={fluid} alt="proj-img"/>*/}
+    <CardBody>
+      <CardTitle>
+        <Link to={path}>
+          {title}
         </Link>
-      </CardBody>
-    </Card>
-)
+      </CardTitle>
+      <CardText>{body}</CardText>
+      <Link
+        to={path}
+        className="btn btn-outline-primary float-right">
+        Click to see more.
+      </Link>
+    </CardBody>
+  </Card>
+);
 
 export default Post;
--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,12 +1,47 @@
 import React from "react";
 import Layout from "../components/layout";
 import SEO from "../components/seo";
+import { graphql, StaticQuery } from 'gatsby';
+import Post from '../components/Post';
 
 const BlogPage = () => (
   <Layout>
     <SEO title="Blog" keywords={[`gatsby`, `application`, `react`]} />
     <h1>Blog</h1>
+    <StaticQuery
+      query={postQuery}
+      render={data => {
+        return (
+          <div>
+            {data.allMarkdownRemark.edges.map(({ node }) => (
+              <Post
+                title={node.frontmatter.title}
+                path={node.frontmatter.path}
+                body={node.excerpt}
+              />
+            ))}
+          </div>
+        )
+      }}
+    />
   </Layout>
 )
+
+const postQuery = graphql`
+  query postQuery {
+    allMarkdownRemark {
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            path
+          }
+          excerpt
+        }
+      }
+    }
+  }
+`
 
 export default BlogPage;
