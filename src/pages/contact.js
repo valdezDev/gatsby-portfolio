66f232b8f08b636042a32d7f4c7118f8c3330bec
Paul Valdez <pvaldez11295@gmail.com>
2019-02-12 17:37:18 -0500

    setting up the contact page and form to link up with netlify

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -6,6 +6,44 @@ const ContactPage = () => (
   <Layout>
     <SEO title="Contact" keywords={[`gatsby`, `application`, `react`]} />
     <h1>Contact</h1>
+					<section>
+						<header>
+							<h2>Get in touch</h2>
+						</header>
+						<div className="content">
+							<p><strong>Auctor commodo</strong> interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque venenatis dolor imperdiet dolor mattis sagittis.</p>
+							<form action="POST" data-netlify="true">
+								<div className="fields">
+									<div className="field half">
+										<input type="text" name="name" id="name" placeholder="Name" />
+									</div>
+									<div className="field half">
+										<input type="email" name="email" id="email" placeholder="Email" />
+									</div>
+									<div className="field">
+										<input type="file" name="myfile" id="myfile" placeholder="Upload File" rows="7" />
+                  </div>
+                  <div className="field">
+										<textarea name="message" id="message" placeholder="Message" rows="7"></textarea>
+                </div>
+                <div className="field">
+                  <div data-netlify-recaptcha="true"></div>
+                </div>
+								</div>
+								<ul className="actions">
+									<li><input type="submit" value="Send Message" className="button primary" /></li>
+								</ul>
+							</form>
+						</div>
+						<footer>
+							<ul className="items">
+								<li>
+									<h3>Email</h3>
+									<a href="#">information@untitled.ext</a>
+								</li>
+							</ul>
+						</footer>
+					</section>
   </Layout>
 )
 
