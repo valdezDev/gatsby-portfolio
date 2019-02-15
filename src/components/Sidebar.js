5df91734ee3a94e4c20619e9366e9f948dc2e1b4
Paul Valdez <pvaldez11295@gmail.com>
2019-02-15 15:07:14 -0500

    built added and started styling the Sidebar component to the single-post template

---
+++ src/components/Sidebar.js
@@ -0,0 +1,78 @@
+import React from 'react'
+import {
+  Card,
+  CardTitle,
+  CardBody,
+  CardText,
+  Form,
+  FormGroup,
+  Input,
+} from 'reactstrap'
+import { graphql, StaticQuery, Link } from 'gatsby'
+import Img from 'gatsby-image'
+
+const Sidebar = () => (
+  <div>  
+    <Card className="sidecard">
+      <CardBody className="sidecard__body">
+        <CardTitle className="text-center text-uppercase mb-3 sidecard__title">
+          Recent Posts
+        </CardTitle>
+        <StaticQuery
+          query={sidebarQuery}
+          render={data => (
+            <div>
+              {data.allMarkdownRemark.edges.map(({ node }) => (
+                <Card key={node.id}>
+                  <Link to={node.fields.slug}>
+                    <Img
+                      className="card-image-top"
+                      fluid={node.frontmatter.image.childImageSharp.fluid}
+                    />
+                  </Link>
+                  <CardBody>
+                    <CardTitle>
+                      <Link to={node.fields.slug}>
+                        {node.frontmatter.title}
+                      </Link>
+                    </CardTitle>
+                  </CardBody>
+                </Card>
+              ))}
+            </div>
+          )}
+        />
+      </CardBody>
+    </Card>
+  </div>
+)
+
+const sidebarQuery = graphql`
+  query sidebarQuery {
+    allMarkdownRemark(
+      sort: { fields: [frontmatter___date], order: DESC }
+      limit: 3
+    ) {
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            image {
+              childImageSharp {
+                fluid(maxWidth: 300) {
+                  ...GatsbyImageSharpFluid
+                }
+              }
+            }
+          }
+          fields {
+            slug
+          }
+        }
+      }
+    }
+  }
+`
+
+export default Sidebar
--- src/templates/single-post.js
+++ src/templates/single-post.js
@@ -1,43 +1,96 @@
 import React from 'react';
 import Layout from '../components/layout';
-import { graphql, Link } from 'gatsby';
+import Sidebar from '../components/sidebar';
+import { graphql } from 'gatsby';
 import SEO from '../components/seo';
-import { Card, CardBody, CardSubtitle } from 'reactstrap';
+import { Card, CardBody, CardSubtitle, Row, Col } from 'reactstrap';
 import Img from 'gatsby-image';
 //import { slugify } from '../util/utilityFunctions';
-//import authors from '../util/authors';
-//import { DiscussionEmbed } from 'disqus-react';
+import { DiscussionEmbed } from 'disqus-react';
 
-const SinglePost = ({ data }) => {
+const SinglePost = ({ data, pageContext }) => {
   
   const post = data.markdownRemark.frontmatter;
+
+  const baseUrl = 'https://valdezdev.tech/';
+
+  const disqusShortname = 'https-valdezdev-tech';
+  const disqusConfig = {
+    identifier: data.markdownRemark.id,
+    title: post.title,
+    url: baseUrl + pageContext.slug
+  };
 
   return (
     <Layout>
       <SEO title={post.title} />
       <h1 className="page-header">{post.title}</h1>
-      <Card className="post-card">
-        <Img
-          className="card-image-top"
-          fluid={post.image.childImageSharp.fluid}
-        />
-        <CardBody className="post-card__body">
-          <CardSubtitle className="post-card__info">
-            <span className="text-info">Posted on {post.date}</span> by {' '}
-            <span className="text-info">{post.author}</span>
-          </CardSubtitle>
-          <div className="post-card__text" dangerouslySetInnerHTML={{ __html: data.markdownRemark.html }} />
-          {/*<ul className="post-tags">
-            {post.tags.map(tag => (
-              <li key={tag}>
-                <Link to={`/tag/${slugify(tag)}`}>
-                  <Badge color="primary">{tag}</Badge>
-                </Link>
-              </li>
-            ))}
-            </ul>*/}
-        </CardBody>
-      </Card>
+      <Row>
+        <Col md="8">
+          <Card className="post-card">
+            <Img
+              className="card-image-top"
+              fluid={post.image.childImageSharp.fluid}
+            />
+            <CardBody className="post-card__body">
+              <CardSubtitle className="post-card__info">
+                <span className="text-info">Posted on {post.date}</span> by {' '}
+                <span className="text-info">{post.author}</span>
+              </CardSubtitle>
+              <div className="post-card__text" dangerouslySetInnerHTML={{ __html: data.markdownRemark.html }} />
+            </CardBody>
+          </Card>
+        </Col>
+        <Col md="4">
+          <Sidebar/>
+        </Col>
+      </Row>
+
+      <h3 className="text-center">
+              Share this post
+      </h3>
+      <div className="text-center social-share-links">
+        <ul>
+          <li>
+            <a
+              href={ 'https://www.facebook.com/sharer/sharer.php?u=' + baseUrl + pageContext.slug }
+              className="facebook"
+              target="_blank"
+              rel="noopener noreferrer">
+              <i className="fab fa-facebook-f fa-2x"/>
+            </a>
+          </li>
+          <li>
+            <a
+              href={ 'https://twitter.com/share?url=' + baseUrl + pageContext.slug + '&text=' + post.title + '&via' + 'twitterHandle'}
+              className="twitter"
+              target="_blank"
+              rel="noopener noreferrer">
+              <i className="fab fa-twitter fa-2x"/>
+            </a>
+          </li>
+          <li>
+            <a
+              href={ 'https://plus.google.com/share?url=' + baseUrl + pageContext.slug }
+              className="google"
+              target="_blank"
+              rel="noopener noreferrer">
+              <i className="fab fa-google fa-2x"/>
+            </a>
+          </li>
+          <li>
+            <a
+              href={ 'https://www.linkedin.com/shareArticle?url=' + baseUrl + pageContext.slug }
+              className="linkedin"
+              target="_blank"
+              rel="noopener noreferrer">
+              <i className="fab fa-linkedin fa-2x"/>
+            </a>
+          </li>
+        </ul>
+      </div>
+      <DiscussionEmbed shortname={disqusShortname} config={disqusConfig} />
+
     </Layout>
   )
 }
