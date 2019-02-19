5c659105fc6611da456362672a2387e4967571a3
Paul Valdez <pvaldez11295@gmail.com>
2019-02-19 18:06:24 -0500

    built and styled hero and header of the index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,17 +3,28 @@ import Layout from "../components/layout";
 import SEO from "../components/seo";
 //import Img from 'gatsby-image'; come back to this.
 import PaulImage from '../images/paul.jpg';
+import HeroImage from '../images/hero-background.jpg';
+import { Button, Card, Col, Container, NavItem, NavLink, Row} from "reactstrap";
+import '../styles/index.scss';
 
 const IndexPage = () => (
   <Layout>
-    <div className="container-fluid">
-      <SEO title="Paul Valdez" keywords={[`gatsby`, `application`, `react`]} />
-      <h1 className="page-header">Paul Valdez</h1>
-      <img src={PaulImage} className="col-md-4 paul-pic" alt="paul-img" />
-      <h4 className="col-offset-3 text-center index-skills">MongoDB | Express | React | Node</h4>
-      <p className="about-me">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cumque impedit explicabo blanditiis provident odit quos, reprehenderit temporibus, iure non necessitatibus porro, at nulla cum vero omnis. Consequuntur laudantium rerum reiciendis at ratione voluptatem, voluptatibus voluptates autem quis deleniti molestias dolorum illo aliquam quidem voluptatum commodi corporis suscipit laboriosam sit vel quam nesciunt tempora illum temporibus! Tenetur laborum quibusdam quasi cum fuga maxime incidunt temporibus est quae eius enim ullam nemo dicta repellendus nihil non ea, pariatur vitae quam, aliquid fugiat. Molestias a laboriosam ducimus quibusdam cum quo adipisci animi nisi perferendis commodi accusamus ea, repellat unde placeat distinctio, alias tenetur delectus architecto similique eum facilis aliquam, tempora illum. Sapiente eaque dolore aperiam maxime quaerat nesciunt itaque nihil temporibus debitis illo saepe voluptatum tempora laborum deleniti molestiae, eum error facere. Cupiditate molestiae omnis aliquam nulla quasi error voluptas excepturi ipsam adipisci architecto harum fuga officiis quidem in non inventore, quae aut.</p>
-    </div>
+    <SEO title="Paul Valdez" keywords={[`paul valdez`, `developer`, `javascript`]} />
+    <Container>
+      <div className="hero-background-0"></div>
+      <div className="hero-background-1"></div>
+    </Container>
+    <Container className="index-intro">
+      <h1 className="index-intro__page-header">Paul Valdez</h1>
+      <img src={PaulImage} className="col-md-4 index-intro__paul-img" alt="paul-img" />
+      <h2 className="index-intro__profession text-center">Full Stack MERN Developer</h2>
+      <NavItem>
+        <Button className="col-md-3 index-intro__button">
+          <NavLink className="index-intro__link" href="/contact">Get in Touch!</NavLink>
+        </Button>
+      </NavItem>
+    </Container>
   </Layout>
-)
+);
 
 export default IndexPage;
