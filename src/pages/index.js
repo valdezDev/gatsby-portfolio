14ae337a6ac2d56c3391255799dc463d57424b4c
Paul Valdez <pvaldez11295@gmail.com>
2019-02-20 17:54:10 -0500

    applied opacity stylings to the navbar, removed footer from layout in previous commit, put the card inside of a row, col on the index page.

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,6 @@ import Layout from "../components/layout";
 import SocialButtons from "../components/SocialButtons";
 import SEO from "../components/seo";
 //import Img from 'gatsby-image'; come back to this.
-import PaulImage from '../images/paul.jpg';
 import { Button, Card, CardTitle, CardSubtitle, Col, Container, NavItem, NavLink, Row} from "reactstrap";
 import '../styles/index.scss';
 
@@ -14,7 +13,8 @@ const IndexPage = () => (
       <div className="hero-background-0"></div>
       <div className="hero-background-1"></div>
     </Container>
-    
+    <Row>
+      <Col sm="12">
       <Card className="index-intro">
         <CardTitle className="index-intro__page-header text-center">Paul Valdez</CardTitle>
         <CardSubtitle className="index-intro__profession text-center">Full Stack MERN Developer</CardSubtitle>
@@ -24,15 +24,9 @@ const IndexPage = () => (
           </Button>
         </NavItem>
         <SocialButtons/>
-      </Card>
-      {/*<h1 className="index-intro__page-header">Paul Valdez</h1>
-      <img src={PaulImage} className="col-md-4 index-intro__paul-img" alt="paul-img" />
-      <h2 className="index-intro__profession text-center">Full Stack MERN Developer</h2>
-      <NavItem>
-        <Button className="col-md-3 index-intro__button">
-          <NavLink className="index-intro__link" href="/contact">Get in Touch!</NavLink>
-        </Button>
-      </NavItem>*/}
+        </Card>
+        </Col>
+    </Row>
   </Layout>
 );
 
