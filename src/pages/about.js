6c6e38e0e16324af82d15827df129a8350999159
Paul Valdez <pvaldez11295@gmail.com>
2019-02-21 13:30:40 -0500

    applied styles to the about page.

--- src/pages/about.js
+++ src/pages/about.js
@@ -10,72 +10,69 @@ const AboutPage = () => (
 
   <Layout>
     <SEO title="Paul Valdez" keywords={[`paul valdez`, `developer`, `javascript`]} />
-    <Card>
+    <h1 className="page-header">About Paul</h1>
+    <Card className="about-card">
       <Row>
-        <Col sm="6">
+        <Col sm="12">
           <Card className="about-img">
-            <CardImg top width="100%" src={PaulImage} alt="Card image cap" />
-            <CardSubtitle>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</CardSubtitle>
+            <CardImg
+              top
+              src={PaulImage}
+              alt="Card image cap"
+              className="about-img__img"
+            />
           </Card>
         </Col>
-        <Col sm="6">
+        <Col sm="12">
           <Card className="about-me">
             <CardBody className="about-me__body">
               <CardTitle className="about-me__title text-center">Who is Paul Valdez?</CardTitle>
-              <CardText>
+              <CardText className="about-me__text col-md-10">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt provident quae ipsam dolores eaque eveniet. Voluptatibus animi excepturi optio earum consectetur eaque rem quisquam accusantium atque, enim dolorum hic! Sint?
               </CardText>
-              <CardText>
+              <CardText className="about-me__text col-md-10">
                 Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cumque fuga ab aliquid libero temporibus. Labore unde maxime nisi adipisci reprehenderit.
               </CardText>
-              <CardText>
+              <CardText className="about-me__text col-md-10">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia ipsum, temporibus saepe unde minus quisquam porro quod eos enim blanditiis corrupti minima quas debitis quis beatae, vitae, neque nemo distinctio perspiciatis eum facilis. Praesentium accusantium voluptate quis ea amet corrupti quo iure fugit voluptatum possimus?
               </CardText>
             </CardBody>
           </Card>
         </Col>
         <Col sm="6">
-          <Card>
-            <CardTitle>Skills</CardTitle>
-            <CardText>
-              <ul>
-                <li>React</li>
-                <li>ExpressJS</li>
-                <li>MongoDB</li>
-                <li>NodeJS</li>
-                <li>Gatsby</li>
+          <Card className="skills col-md-11">
+            <CardTitle className="skills__title text-center"># Skills</CardTitle>
+            <CardText className="skills__list-wrapper">
+              <ul className="skills__list">
+                <li>// MongoDB</li>
+                <li>// ExpressJS</li>
+                <li>// React</li>
+                <li>// NodeJS</li>
+                <li>// REST</li>
+                <li>// Gatsby</li>
+                <li>// jQuery</li>
+                <li>// Sass</li>
+              </ul>
+            </CardText>
+          </Card>
+        </Col>
+        <Col sm="6">
+          <Card className="tools col-md-11">
+            <CardTitle className="tools__title text-center"># Tools</CardTitle>
+            <CardText className="tools__list-wrapper">
+              <ul className="tools__list">
+                <li>Git (Bash), <a href="https://github.com/valdezDev" target="_blank" rel="noopener noreferrer">Github</a></li>
+                <li>Visual Studio Code</li>
+                <li>PostMan</li>
+                <li>JEST</li>
+                <li>mLab</li>
+                <li>Kanban Flow</li>
               </ul>
             </CardText>
           </Card>
         </Col>
       </Row>
     </Card>
-    {/*<Container>
-      <Row>
-        
-          <Card>
-            <CardImg top width="100%" src={PaulImage} alt="Card image cap" />
-            <CardSubtitle>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Obcaecati, illum.</CardSubtitle>
-          </Card>
-        
-      </Row>
-      <Row>
-        <Col>
-          <Card>
-            <CardTitle>Who is Paul Valdez?</CardTitle>
-            <CardText>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt provident quae ipsam dolores eaque eveniet. Voluptatibus animi excepturi optio earum consectetur eaque rem quisquam accusantium atque, enim dolorum hic! Sint?
-            </CardText>
-            <CardText>
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cumque fuga ab aliquid libero temporibus. Labore unde maxime nisi adipisci reprehenderit.
-            </CardText>
-            <CardText>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia ipsum, temporibus saepe unde minus quisquam porro quod eos enim blanditiis corrupti minima quas debitis quis beatae, vitae, neque nemo distinctio perspiciatis eum facilis. Praesentium accusantium voluptate quis ea amet corrupti quo iure fugit voluptatum possimus?
-            </CardText>
-          </Card>
-        </Col>
-      </Row>
-    </Container>*/}
   </Layout>
 );
 export default AboutPage;
