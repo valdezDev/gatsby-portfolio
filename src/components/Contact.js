e36b35d6341528f7207d24f2acec9ecba178654d
Paul Valdez <pvaldez11295@gmail.com>
2019-02-26 16:36:44 -0500

    rewriting contact form for troubleshooting

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -46,27 +46,13 @@ const Contact = () => (
                 <li><input className="contact-card__send-message btn btn-outline" type="submit" value="Send Message" /></li>
               </ul>
             </form>*/}
-            <form name="contact" method="POST" data-netlify="true" data-netlify-honeypot="bot-field" className="contact-card__form">
-            {/* You still need to add the hidden input with the form name to your JSX form */}
-            <input type="hidden" name="contact-form" value="contact" />
-            <div className="contact-card__fields">
-                <div className="contact-card__field half">
-                  <input className="contact-card__name col-md-3" type="text" name="name" id="name" placeholder="Name" />
-                </div>
-                <div className="contact-card__field half">
-                  <input className="contact-card__email col-md-3" type="email" name="email" id="email" placeholder="email@email.com" />
-                </div>
-                <div className="contact-card__field">
-                  <textarea className="contact-card__message col-md-10" name="message" id="message" rows="8" placeholder="Start typing..." ></textarea>
-              </div>
-              <div className="contact-card__field">
-                <div data-netlify-recaptcha="true"></div>
-              </div>
-              </div>
-              <ul className="contact-card__actions">
-                <li><input className="contact-card__send-message btn btn-outline" type="submit" value="Send Message" /></li>
-              </ul>
-          </form>
+            <form method="post" action="#" data-netlify="true" name="contact-form-test">
+  <input type="hidden" name="form-name-test" value="contact" />
+  <input type="text" name="name" id="name" required />
+  <input type="text" name="email" id="email" required />
+  <textarea name="message" id="message" rows="4" required />
+  <input type="submit" value="Send Message" />
+</form>
           </CardBody>
         </Card>
       </Col>
