c11a7c853e072bf61f9e2f8bd15721e7998cf250
Paul Valdez <pvaldez11295@gmail.com>
2019-03-06 16:37:53 -0500

    applied temporary styles to the success and contact page

--- src/pages/success.js
+++ src/pages/success.js
@@ -4,23 +4,22 @@ import Layout from '../components/layout';
 
 const Success = (props) => (
   <Layout>
-      <Helmet>
-          <title>Success! Thank You.</title>
-          <meta name="description" content="Success Page" />
-      </Helmet>
+    <Helmet>
+      <title>Success! Thank You.</title>
+      <meta name="description" content="Success Page" />
+    </Helmet>
 
-      <div id="main" className="alt">
-          <section id="one">
-              <div className="inner">
-                  <header className="major">
-                      <h1>Success/Thank You Page</h1>
-                  </header>
-                  <span className="image main"><img src="http://images.adagio.com/images2/custom_blends/139293.jpg" alt="jake" /></span>
-                  <p>Thank you for contacting us!</p>
-              </div>
-          </section>
-      </div>
+    <div id="main" className="success">
+      <section id="one">
+        <div className="success__inner">
+          <header className="success__major">
+            <h1 className="success__major" color="red">Success! 📫😇</h1>
+          </header>
+          <p className="text-center success__small">Thank you for taking the time to reach out!</p>
+        </div>
+      </section>
+    </div>
   </Layout>
-)
+);
 
-export default Success
+export default Success;
