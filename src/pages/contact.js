c6a2f968af3b4897471ccf1d507900dd3749d30b
Paul Valdez <pvaldez11295@gmail.com>
2019-04-06 19:38:41 -0400

    refactored contact form code

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -2,12 +2,14 @@ import React from "react";
 import Layout from "../components/layout";
 import SEO from "../components/seo";
 import Contact from '../components/Contact';
+import ContactForm from '../components/ContactForm';
 import '../styles/index.scss';
 
 const ContactPage = () => (
   <Layout>
     <SEO title="Contact" keywords={[`gatsby`, `application`, `react`]} />
     <Contact />
+    <ContactForm />
   </Layout>
 );
 
