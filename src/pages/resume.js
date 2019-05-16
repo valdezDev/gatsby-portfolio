3545708b7c004a4d867538b656fd2beb5adff776
Paul Valdez <pvaldez11295@gmail.com>
2019-05-16 19:02:08 -0400

    applied similar terminal-looking styles to the projects

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -4,7 +4,7 @@ import Layout from "../components/layout"
 
 
 const Resume = () => (
-    <iframe src="https://drive.google.com/file/d/0BwoVa35mkpHiNlRYN1VPT0NxQW5uQmZrUjNoSmxzNjJtWE5J/preview" width="100%" height="1000"></iframe>
+    <iframe src="https://drive.google.com/file/d/1VkasX_EI_sNZGLOJ7FaSR6eo1PuYDCKM/view?usp=sharing" width="100%" height="1000"></iframe>
 )
 
 export default Resume;
